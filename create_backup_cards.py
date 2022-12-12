08eafce8b71c5c81664fb8e455187e234d87bd45
plebg <plebeiusgaragicus@gmail.com>
2022-12-12 15:58:15 -0800

    not fixed to max of 3 keys

--- create_backup_cards.py
+++ create_backup_cards.py
@@ -5,23 +5,7 @@ import qrcode
 import string
 
 
-wallet_name = input("Enter wallet name (leave blank to use default -> 'Multisig Backup'): ")
-if wallet_name == "":
-    wallet_name = "Multisig Backup"
-
-descriptor = ""
-while descriptor == "":
-    descriptor = input("Paste the 2/3 multisig wallet descriptor from Sparrow Wallet: ")
-
-
-qr_density = input("Entire desired qr code density as an integer ('21'=S-21x21, '25'=M-25x25, '29'=L-29x29). Leave blank to use default (M): ")
-if qr_density == "":
-    qr_density = 25
-else:
-    qr_density = int(qr_density)
-
 def create_card_template(c, width = 3.5*inch, height = 2.5*inch):
-    
     # calculate border_width
     border_width = .05*height
     top_border_width = .075*height
@@ -69,9 +53,6 @@ def create_card_template(c, width = 3.5*inch, height = 2.5*inch):
 
 
 def draw_qr_code_template(c, size, loc_x = 0, loc_y = 0, num_cols_rows = 25):
-
-    
-
     # determine the width and height of row and column labels
     row_label_width = 1/20*size
     col_label_height = 1/20*size
@@ -176,186 +157,175 @@ def draw_qr_code_template(c, size, loc_x = 0, loc_y = 0, num_cols_rows = 25):
     c.translate(-loc_x-row_label_width, -loc_y)
     return
 
-# Split out each key info for descriptor
-descriptor = descriptor.split(',')
 
-key_info_1 = descriptor[1]
-key_info_2 = descriptor[2]
-key_info_3 = descriptor[3].split(')')[0]
+def main():
 
+    wallet_name = input("Enter wallet name (leave blank to use default -> 'Multisig Backup'): ")
+    if wallet_name == "":
+        wallet_name = "Multisig Backup"
 
-# Split out information for each key
-fingerprint1= key_info_1.split(']')[0][1:9]
-xpub1 = key_info_1.split(']')[1].split('/')[0]
-derivation1 = 'm/'+key_info_1.split(']')[0][10:].replace('h','\' ')
-key_info_1_qrcode = qrcode.make(key_info_1)
-key_info_1_qrcode.save("qrcodes/key_info_1_qrcode.png")
+    descriptor = ""
+    while descriptor == "":
+        descriptor = input("Paste the 2/3 multisig wallet descriptor from Sparrow Wallet: ")
 
-fingerprint2= key_info_2.split(']')[0][1:9]
-xpub2 = key_info_2.split(']')[1].split('/')[0]
-derivation2 = 'm/'+key_info_2.split(']')[0][10:].replace('h','\' ')
-key_info_2_qrcode = qrcode.make(key_info_2)
-key_info_2_qrcode.save("qrcodes/key_info_2_qrcode.png")
-
-fingerprint3= key_info_3.split(']')[0][1:9]
-xpub3 = key_info_3.split(']')[1].split('/')[0]
-derivation3 = 'm/'+key_info_3.split(']')[0][10:].replace('h','\' ')
-key_info_3_qrcode = qrcode.make(key_info_3)
-key_info_3_qrcode.save("qrcodes/key_info_3_qrcode.png")
-
-# Make a dictionary to loop through later
-wallet_description = {"key1":
-                            {'key_number':1,
-                             'fp': fingerprint1,
-                             'xpub': xpub1,
-                             'derivation': derivation1,
-                             'qr_code': 'qrcodes/key_info_1_qrcode.png'},
-                      "key2":
-                            {'key_number':2,
-                             'fp': fingerprint2,
-                             'xpub': xpub2,
-                             'derivation': derivation2,
-                             'qr_code': 'qrcodes/key_info_2_qrcode.png'},
-                      "key3":
-                            {'key_number':3,
-                             'fp': fingerprint3,
-                             'xpub': xpub3,
-                             'derivation': derivation3,
-                             'qr_code': 'qrcodes/key_info_3_qrcode.png'}}
-
-# Create pdf canvas and set starting point
-c = canvas.Canvas(wallet_name.replace(" ","_")+"_multi_sig_backup.pdf", pagesize = letter)
-c.translate(0.5*inch, 10.5*inch)
-
-num_keys = 3
-
-for i in range(num_keys):
-
-    # create template and grab dimensions
-    height, width, top_border_width, border_width = create_card_template(c)
-    top_padding = border_width
-    object_padding = .5*border_width
-    text_box_height = 1.1*border_width
-    font_size = 0.8*text_box_height
-
-    # Draw Qr Code
-    qr_size = height - top_border_width - object_padding*2 - top_padding - text_box_height - border_width
-    qr_x_loc = width - border_width - object_padding - qr_size
-    qr_y_loc = -height + border_width + 2*object_padding + text_box_height
-
-    draw_qr_code_template(c, qr_size, qr_x_loc, qr_y_loc, qr_density)
-
-    
-    c.setLineWidth(1)
-    c.setFont('Helvetica', font_size)
-    
-    # Draw key numbering
-    key_num_box_width = (qr_size-object_padding)*1/3
-    key_size = 0.8*text_box_height
-    key_num_x_loc = width - border_width - object_padding - key_num_box_width
-    key_num_y_loc = -height + border_width + object_padding
-
-    c.drawImage('imgs/keys.jpg',key_num_x_loc+2.5,key_num_y_loc+ text_box_height/2 - key_size/2,width=key_size,height=key_size)
-    c.roundRect(key_num_x_loc, key_num_y_loc, key_num_box_width, text_box_height, radius=text_box_height/2)
-    c.drawCentredString(key_num_x_loc+key_size/2 + key_num_box_width/2, key_num_y_loc+ text_box_height/4.5, str(wallet_description['key'+str(i+1)]['key_number'])+"/3")
-
-
-
-    # Draw fingerprint
-    fp_box_width = (qr_size-object_padding)*2/3
-    fp_size = .8*text_box_height
-    fp_x_loc = width - border_width - 2*object_padding - key_num_box_width - fp_box_width
-    fp_y_loc = -height + border_width + object_padding
-    
-    c.drawImage('imgs/fingerprint.jpg',fp_x_loc+2.5,fp_y_loc + text_box_height/2 - fp_size/2,width=fp_size,height=fp_size)
-    c.roundRect(fp_x_loc,fp_y_loc,fp_box_width,text_box_height, radius=text_box_height/2)
-    c.drawCentredString(fp_x_loc+fp_size/2+ (fp_box_width)/2,fp_y_loc + text_box_height/4.5,wallet_description['key'+str(i+1)]['fp'])
-
-    # Draw Title
-    title_box_width = width - 2*border_width-3*object_padding-qr_size
-    title_x_loc = border_width+object_padding
-    title_y_loc = -top_border_width-top_padding
-    title_box_height = 1.25*text_box_height
-    title_font_size = .8*title_box_height
-    c.setFont('Helvetica-Bold', title_font_size)
-
-    c.roundRect(title_x_loc,title_y_loc,title_box_width,-title_box_height, radius=text_box_height/4)
-    c.drawCentredString(border_width+object_padding+title_box_width/2, -top_border_width-top_padding-title_box_height+ title_box_height/4.5,wallet_name)
-
-    # Draw Spots for 12 words
-    to = c.beginText(border_width+object_padding,-top_border_width-top_padding-title_box_height-object_padding-.6*text_box_height)
-    secret_words_font_size = font_size*1.15
-    to.setFont('Helvetica',secret_words_font_size)
-    to.setFillColor('black')
-    for i in range(12):
-        if i < 9:
-            to.textLine(text=str(i+1)+":________________")
-        else:
-            to.textLine(text=str(i+1)+":_______________")
-
-    c.drawText(to)
-
-
-    c.translate(0,-2.75*inch)
-
-
-
-# Start new page and set starting point
-c.showPage()
-c.translate(4.5*inch, 10.5*inch)
-
-for key in wallet_description:
-
-    if wallet_description[key]["key_number"] != 3:
-        c.translate(0,-2.75*inch)
+    qr_density = input("Entire desired qr code density as an integer ('21'=S-21x21, '25'=M-25x25, '29'=L-29x29). Leave blank to use default (M): ")
+    if qr_density == "":
+        qr_density = 25
     else:
-        c.translate(0,+2*2.75*inch)
+        qr_density = int(qr_density)
+
+    descriptor = descriptor.split(',')
+    num_keys = len(descriptor)-1
+    print("Number of keys: " + str(num_keys))
+
+    key_info = []
+    for i in range(num_keys):
+        d = descriptor[i+1].split('[')[1].split('*')[0] + '*'
+        print(f"This round {d}")
+
+        new = {
+            "fingerprint": d.split(']')[0][0:8],
+            "xpub": d.split(']')[1].split('/')[0],
+            "derivation": 'm/'+d.split(']')[0][9:].replace('h','\' ')
+        }
+        print( new )
+        key_info.append( new )
+
+        key_info_qrcode = qrcode.make( d )
+        key_info_qrcode.save(f"qrcodes/key_info_{i+1}_qrcode.png")
 
 
-    height, width, top_border_width, border_width = create_card_template(c)
+    # Create pdf canvas and set starting point
+    c = canvas.Canvas(wallet_name.replace(" ","_")+"_multi_sig_backup.pdf", pagesize = letter)
+    c.translate(0.5*inch, 10.5*inch)
 
-    
+    # for i in range(num_keys):
+    for i, key in enumerate(key_info):
 
-    # Draw Title
-    title_box_width = width - 2*border_width-3*object_padding-qr_size
-    title_x_loc = border_width+object_padding
-    title_y_loc = -top_border_width-top_padding
-    title_box_height = 1.25*text_box_height
-    title_font_size = .8*title_box_height
-    c.setFont('Helvetica', font_size)
+        # create template and grab dimensions
+        height, width, top_border_width, border_width = create_card_template(c)
+        top_padding = border_width
+        object_padding = .5*border_width
+        text_box_height = 1.1*border_width
+        font_size = 0.8*text_box_height
 
-    # Add xpub title with key logo
-    c.drawImage('imgs/keys.jpg',title_x_loc+2.5,title_y_loc- text_box_height/2 - key_size/2,width=key_size,height=key_size)
-    c.roundRect(title_x_loc,title_y_loc,title_box_width,-text_box_height, radius=text_box_height/2)
-    c.drawCentredString(border_width+object_padding+title_box_width/2, -top_border_width-top_padding-text_box_height+ text_box_height/4.5,"#"+str(wallet_description[key]['key_number'])+" xpub")
+        # Draw Qr Code
+        qr_size = height - top_border_width - object_padding*2 - top_padding - text_box_height - border_width
+        qr_x_loc = width - border_width - object_padding - qr_size
+        qr_y_loc = -height + border_width + 2*object_padding + text_box_height
 
-    # Add xpub qr code
-    c.drawImage(wallet_description[key]['qr_code'],qr_x_loc-.025*qr_size,qr_y_loc-.025*qr_size,width = qr_size*1.05,height=qr_size*1.05)
+        draw_qr_code_template(c, qr_size, qr_x_loc, qr_y_loc, qr_density)
 
-    # Add Xpub
-    to = c.beginText(border_width+object_padding,-top_border_width-top_padding-title_box_height-object_padding-.6*text_box_height)
-    xpub_font_size = font_size*1.4
-    to.setFont('Helvetica',xpub_font_size)
-    to.setFillColor('black')
-    i = 0
-    for i in wallet_description[key]['xpub']:
-        if to.getCursor()[0] >= (border_width+title_box_width):
-            to.moveCursor(0,13)
-        to.textOut(i)
-        to.setFont('Helvetica',xpub_font_size/2)
-        to.textOut(" ")
+        
+        c.setLineWidth(1)
+        c.setFont('Helvetica', font_size)
+        
+        # Draw key numbering
+        key_num_box_width = (qr_size-object_padding)*1/3
+        key_size = 0.8*text_box_height
+        key_num_x_loc = width - border_width - object_padding - key_num_box_width
+        key_num_y_loc = -height + border_width + object_padding
+
+        c.drawImage('imgs/keys.jpg',key_num_x_loc+2.5,key_num_y_loc+ text_box_height/2 - key_size/2,width=key_size,height=key_size)
+        c.roundRect(key_num_x_loc, key_num_y_loc, key_num_box_width, text_box_height, radius=text_box_height/2)
+        c.drawCentredString(key_num_x_loc+key_size/2 + key_num_box_width/2, key_num_y_loc+ text_box_height/4.5, str(i+1) + f"/{num_keys}")
+
+
+
+        # Draw fingerprint
+        fp_box_width = (qr_size-object_padding)*2/3
+        fp_size = .8*text_box_height
+        fp_x_loc = width - border_width - 2*object_padding - key_num_box_width - fp_box_width
+        fp_y_loc = -height + border_width + object_padding
+        
+        c.drawImage('imgs/fingerprint.jpg',fp_x_loc+2.5,fp_y_loc + text_box_height/2 - fp_size/2,width=fp_size,height=fp_size)
+        c.roundRect(fp_x_loc,fp_y_loc,fp_box_width,text_box_height, radius=text_box_height/2)
+        c.drawCentredString(fp_x_loc+fp_size/2+ (fp_box_width)/2,fp_y_loc + text_box_height/4.5, key['fingerprint'])
+
+        # Draw Title
+        title_box_width = width - 2*border_width-3*object_padding-qr_size
+        title_x_loc = border_width+object_padding
+        title_y_loc = -top_border_width-top_padding
+        title_box_height = 1.25*text_box_height
+        title_font_size = .8*title_box_height
+        c.setFont('Helvetica-Bold', title_font_size)
+
+        c.roundRect(title_x_loc,title_y_loc,title_box_width,-title_box_height, radius=text_box_height/4)
+        c.drawCentredString(border_width+object_padding+title_box_width/2, -top_border_width-top_padding-title_box_height+ title_box_height/4.5,wallet_name)
+
+        # Draw Spots for 12 words
+        to = c.beginText(border_width+object_padding,-top_border_width-top_padding-title_box_height-object_padding-.6*text_box_height)
+        secret_words_font_size = font_size*1.15
+        to.setFont('Helvetica',secret_words_font_size)
+        to.setFillColor('black')
+        for i in range(12):
+            if i < 9:
+                to.textLine(text=str(i+1)+":________________")
+            else:
+                to.textLine(text=str(i+1)+":_______________")
+
+        c.drawText(to)
+
+        c.translate(0,-2.75*inch)
+
+
+    # Start new page and set starting point
+    c.showPage()
+    c.translate(4.5*inch, 10.5*inch)
+
+    for i, key in enumerate(key_info):
+
+        # if not the last key...
+        if i != len(key_info):
+            c.translate(0,-2.75*inch)
+        else:
+            c.translate(0,+2*2.75*inch)
+
+        height, width, top_border_width, border_width = create_card_template(c)
+
+
+        # Draw Title 
+        title_box_width = width - 2*border_width-3*object_padding-qr_size
+        title_x_loc = border_width+object_padding
+        title_y_loc = -top_border_width-top_padding
+        title_box_height = 1.25*text_box_height
+        title_font_size = .8*title_box_height
+        c.setFont('Helvetica', font_size)
+
+        # Add xpub title with key logo
+        c.drawImage('imgs/keys.jpg',title_x_loc+2.5,title_y_loc- text_box_height/2 - key_size/2,width=key_size,height=key_size)
+        c.roundRect(title_x_loc,title_y_loc,title_box_width,-text_box_height, radius=text_box_height/2)
+        c.drawCentredString(border_width+object_padding+title_box_width/2, -top_border_width-top_padding-text_box_height+ text_box_height/4.5,"#"+str(i+1)+" xpub")
+
+        # Add xpub qr code
+        c.drawImage(f"qrcodes/key_info_{i+1}_qrcode.png", qr_x_loc-.025*qr_size,qr_y_loc-.025*qr_size,width = qr_size*1.05,height=qr_size*1.05)
+
+        # Add Xpub
+        to = c.beginText(border_width+object_padding,-top_border_width-top_padding-title_box_height-object_padding-.6*text_box_height)
+        xpub_font_size = font_size*1.4
         to.setFont('Helvetica',xpub_font_size)
+        to.setFillColor('black')
+        i = 0
+        for i in key['xpub']:
+            if to.getCursor()[0] >= (border_width+title_box_width):
+                to.moveCursor(0,13)
+            to.textOut(i)
+            to.setFont('Helvetica',xpub_font_size/2)
+            to.textOut(" ")
+            to.setFont('Helvetica',xpub_font_size)
 
 
-   
+        c.drawText(to)
 
-    c.drawText(to)
+        # Add derivation path
+        c.setFont('Helvetica',font_size)
+        c.roundRect(fp_x_loc, fp_y_loc, qr_size, text_box_height, radius=text_box_height/2)
+        c.drawCentredString(fp_x_loc+(qr_size)/2, fp_y_loc + text_box_height/4.5, key['derivation'])
 
-    
+    c.save()
 
-    # Add derivation path
-    c.setFont('Helvetica',font_size)
-    c.roundRect(fp_x_loc,fp_y_loc,qr_size,text_box_height, radius=text_box_height/2)
-    c.drawCentredString(fp_x_loc+(qr_size)/2,fp_y_loc + text_box_height/4.5,wallet_description[key]['derivation'])
 
-c.save()
+
+
+if __name__ == "__main__":
+    main()
